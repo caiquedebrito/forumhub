aa180adba0276b3846ed96e1064daa6c3e9a2af6
Caique <caiquedebritoo@gmail.com>
2024-07-04 16:49:06 -0300

    add list topics route
    - pagination
    - sort
    - filter by course and year

--- src/main/java/com/alura/forumhub/controller/TopicController.java
+++ src/main/java/com/alura/forumhub/controller/TopicController.java
@@ -6,12 +6,13 @@ import com.alura.forumhub.topic.Topic;
 import com.alura.forumhub.topic.TopicRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -30,5 +31,25 @@ public class TopicController {
         var uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DetailsTopicData(topic));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DetailsTopicData>> list(
+            @RequestParam(required = false, name = "course") String course,
+            @RequestParam(required = false, name = "year") String year,
+            @PageableDefault(size = 10, sort = {"createdAt"}, direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<DetailsTopicData> topics;
+        if (course == null && year == null) {
+            topics = topicRepository.findAll(pageable).map(DetailsTopicData::new);
+        } else if (course == null) {
+            topics = topicRepository.findByYear(year, pageable);
+        } else if (year == null) {
+            topics = topicRepository.findByCourse(course, pageable);
+        } else {
+            topics = topicRepository.fingByYearAnByCourse(year, course, pageable);
+        }
+
+        return ResponseEntity.ok(topics);
     }
 }
